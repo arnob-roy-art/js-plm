306a221863039c03350171aaae4bc0f224a37f89
arnob-roy-art <arnob20121999@gmail.com>
2021-03-17 06:36:34 -0700

    first commit

--- aside.js
+++ aside.js
@@ -4,5 +4,5 @@ function calculation(){
     var side3= Number(document.getElementById("third").value);
     var sum= (side1+side2+side3)/2;
     var area =Math.sqrt(sum*(sum-side1)*(sum-side2)*(sum-side3));
-    console.log(area);
+    document.write(area);
 }
